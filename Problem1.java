4ef22398dc3a31ce928d58b88256b92664384524
Midiel Rodriguez <midiel305@gmail.com>
2019-01-08 19:20:51 -0500

    Removed personal info

--- Problem1.java
+++ Problem1.java
@@ -1,7 +1,7 @@
 /*********************************************************************
 Purpose/Description: This program finds a number "x" inside a sorted array
                      of integers.
-Author’s Panther ID: 1735870
+Author’s Panther ID: xxxxxxx
 Certification:
 I hereby certify that this work is my own and none of it is the work of
 any other person.
